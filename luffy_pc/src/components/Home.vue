0df2f4d3d8b6c39b6f9d9aaa87b6c063585b771d
wanncye <535593370@qq.com>
2022-04-14 18:12:58 +0800

    前端首页：包括导航栏、页脚、轮播图

--- luffy_pc/src/components/Home.vue
+++ luffy_pc/src/components/Home.vue
@@ -1,13 +1,28 @@
 <template>
-    <div id="home">
-      首页
+    <div class="home">
+      <Header></Header>
+      <Banner></Banner>
+      <Footer></Footer>
     </div>
 </template>
 
 <script>
-    export default {
-    name: 'Home'
-    }
+  import Header from "./common/Header"
+  import Banner from "./common/Banner"
+  import Footer from "./common/Footer"
+  export default {
+      name: "Home",
+      data(){
+        return {}
+      },
+      methods:{
+      },
+      components:{
+        Header,
+        Footer,
+        Banner,
+      }
+  }
 </script>
 
 <style scoped>
