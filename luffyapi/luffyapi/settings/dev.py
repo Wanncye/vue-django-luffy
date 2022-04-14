ad390101d1b11eef6a3eeaece43a3c00c5388ae0
wanncye <535593370@qq.com>
2022-04-14 15:00:36 +0800

    配置数据库

--- luffyapi/luffyapi/settings/dev.py
+++ luffyapi/luffyapi/settings/dev.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'luffyapi.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'luffy_user',
+        'PASSWORD': 'luffy',
+        'NAME': 'luffy',
     }
 }
 
