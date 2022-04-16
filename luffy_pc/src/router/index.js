d2012a5e8d4c3f939105ec27fba9772f88e980e2
wanncye <535593370@qq.com>
2022-04-16 18:57:36 +0800

    前端：注册页面

--- luffy_pc/src/router/index.js
+++ luffy_pc/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
 import Login from '../components/Login'
+import Register from '../components/Register'
 
 Vue.use(Router)
 
@@ -22,6 +23,12 @@ export default new Router({
       path: '/user/login',
       name: 'Login',
       component: Login
+    }
+    ,
+    {
+      path: '/user/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
