edbb122913a480e6f0a3335c4e1a66bab3e69cc9
wanncye <535593370@qq.com>
2022-04-18 00:29:37 +0800

    前端：免费课前端页面，因为是从后端传数据过来，因此还显示不全

--- luffy_pc/src/router/index.js
+++ luffy_pc/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '../components/Home'
 import Login from '../components/Login'
 import Register from '../components/Register'
+import Course from '../components/Course'
 
 Vue.use(Router)
 
@@ -23,12 +24,16 @@ export default new Router({
       path: '/user/login',
       name: 'Login',
       component: Login
-    }
-    ,
+    },
     {
       path: '/user/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/courses',
+      name: 'Course',
+      component: Course
     }
   ]
 })
